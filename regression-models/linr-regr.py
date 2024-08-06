5903bd1744ecdb7f3e015ba996ba2484c52cce79
A13x <73394181+0rang3Ta66y@users.noreply.github.com>
2024-08-06 10:16:52 -0500

    Update linr-regr.py

--- regression-models/linr-regr.py
+++ regression-models/linr-regr.py
@@ -10,18 +10,33 @@ from scikit-learn import LinearRegression
 #Ploting data and stuff
 import seaborn as sns
 
+#Class for the AI bot and all the functions that will train it and stuff
 class linr_reg():
+    #Initialize the class
     def __init__():
         pass
-    def data_prep():
-        pass
-    def data_split():
-        pass
-    def train_mlai():
-        pass
-    def validate_data():
-        pass
-    def test_data():
-        pass
+        
+    #Class that has functions to prepare the data
+    class data_prep():
+        #Prep the data by importing it and stuff
+        def data_imprt():
+            pass
+        #Split the data into train, validate, and testing data
+        def data_split():
+            pass
+            
+    #Start training the AI with the data and start to validate it 
+    class train():
+        #Train the AI using the training dataset
+        def train_mlai():
+            pass
+        #Validate the AI and tweak andything if its getting stuff wrong
+        def validate_data():
+            pass
+        #Test the ai with the testing data set 
+        def test_data():
+            pass
+            
+    #Final function so that you can give it data and some other things to do everything in one function, preditcs what will happen
     def predict():
         pass
