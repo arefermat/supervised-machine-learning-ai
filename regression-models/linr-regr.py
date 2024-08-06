d0423706d6614f6db7a29fef36b5c12bf854693f
A13x <73394181+0rang3Ta66y@users.noreply.github.com>
2024-08-06 10:05:58 -0500

    Update linr-regr.py

--- regression-models/linr-regr.py
+++ regression-models/linr-regr.py
@@ -1,1 +1,27 @@
+#Basic for python and fundamentel for Machine learning algorithms
+import numpy as np
 
+#More math and open csv files and suck, also works with data frames
+import pandas as pd
+
+#This imports a simple LinearRegresion model so i can test scikita vs mine
+from scikit-learn import LinearRegression 
+
+#Ploting data and stuff
+import seaborn as sns
+
+class linr_reg():
+    def __init__():
+        pass
+    def data_prep():
+        pass
+    def data_split():
+        pass
+    def train_mlai():
+        pass
+    def validate_data():
+        pass
+    def test_data():
+        pass
+    def predict():
+        pass
